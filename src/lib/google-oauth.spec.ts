720bd02152970764de2cc8bea6a90861f3871422
marco <marcocimolai49@gmail.com>
2022-07-03 13:10:26 +0200

    Change package name

--- src/lib/google-oauth.spec.ts
+++ src/lib/google-oauth.spec.ts
@@ -16,7 +16,7 @@ test('should return an auth token from google', async (t) => {
   t.not(token, undefined);
 });
 
-test('should not verify idToken because expired', async (t) => {
+test('should not verify idToken because invalid', async (t) => {
   await verifyIdToken(
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vdGVzdC1wcm9qZWN0LWlkIiwiYXVkIjoic2RzZHNkc2RzZCIsImF1dGhfdGltZSI6MTY1NTkyNDcxNCwidXNlcl9pZCI6InNkc2RzZHNkc2RzZCIsInN1YiI6InNkc2RzZHNkIiwiaWF0IjoxNjU1OTI0NzE0LCJleHAiOjE2NTU5MjgzMTQsImVtYWlsIjoidGVzdEBlbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsidGVzdEBlbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.3PkmhTW1LaP3lGPnBat2N770TqEE026Xhp2whzbltJo'
   ).catch((e) => {
